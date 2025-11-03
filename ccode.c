94da6aa53c1c9a60f06b1aaf34866c293085161a
MZ181-bit <maazjuha181@gmail.com>
2025-11-03 20:18:43 +0530

    Implement bubble sort in ccode.c

---
+++ ccode.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+  int arr[] = {5, 2, 4, 1, 3}, i, j, temp;
+  for (i = 0; i < 5; i++) {
+    for (j = i + 1; j < 5; j++) {
+      if (arr[i] > arr[j]) {
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  for (i = 0; i < 5; i++)
+    printf("%d ", arr[i]);
+  return 0;
+}
